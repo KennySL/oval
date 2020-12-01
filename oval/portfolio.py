ac7633dfa6feaec1b781171aa05f9b01329caa95
kinsa <lks1596357@gmail.com>
2020-11-30 22:25:04 -0500

    Add val date to Portfolio class

--- oval/portfolio.py
+++ oval/portfolio.py
@@ -22,6 +22,7 @@ portfolio Listing
 
 import pandas as pd
 import plotly.express as px
+from copy import deepcopy as _deepcopy
 
 
 class Position:
@@ -45,9 +46,8 @@ class Position:
     def shares(self):
         return self._shares
 
-    @shares.setter
-    def update_shares(self, new_shares):
-        self._shares = new_shares
+    def update_shares(self, change_in_shares):
+        self._shares += change_in_shares
         self._value = self._calculate_value()
 
     @property
@@ -57,12 +57,17 @@ class Position:
     def _calculate_value(self):
         return self.shares * self.asset.price
 
+    def copy(self):
+        return _deepcopy(self)
+
 
 class Portfolio:
     def __init__(self, positions):
         self._positions = {pos.asset.ticker: pos for pos in positions}
         self._summary = self._summarize()
         self._value = self.summary["value"].sum()
+        self._time_idx = self.get_longest_time_index()
+        self._val_date = self.time_idx[-1]
 
     @property
     def positions(self):
@@ -76,7 +81,15 @@ class Portfolio:
     def value(self):
         return self._value
 
-    def update(self, new_positions):
+    @property
+    def time_idx(self):
+        return self._time_idx
+
+    @property
+    def val_date(self):
+        return self._val_date
+
+    def update(self, change_in_positions):
         """update positions in the portfolio. In addition, reflect
         summary and value accordingly.
 
@@ -88,10 +101,15 @@ class Portfolio:
         =======
 
         """
+
+        # <TODO> need fix, the change in position is not
+        # reflected on portfolio level.
         _new_positions = {pos.asset.ticker: pos for pos in new_positions}
         self._positions.update(_new_positions)
         self._summary = self._summarize()
         self._value = self.summary["value"].sum()
+        self._time_idx = self.get_longest_time_index()
+        self._val_date = self.time_idx[-1]
 
     def _summarize(self):
         """Internal function that produce a summary of the portfolio."""
@@ -133,3 +151,36 @@ class Portfolio:
         if topic == "Weights":
             fig = px.pie(df, values="weight", names=df.index, title="Weights")
             fig.show()
+
+    def get_longest_time_index(self):
+        """return the longest time index given the positions in the portfolio.
+
+        Parameters
+        ==========
+
+        Returns
+        =======
+        longest time index given the positions in the portfolio
+        """
+        keys = []
+        start_dates = []
+
+        for _key, _pos in self.positions.items():
+            try:
+                keys.append(_key)
+                start_dates.append(_pos.asset.start_date)
+            except AttributeError:
+                # for assets with no start date, pass
+                pass
+
+        # figure out the earliest start dates
+        earliest_start_date = start_dates[0]
+
+        for date in start_dates:
+            if date < earliest_start_date:
+                earliest_start_date = date
+
+        # figure out the key by earliest start date
+        earliest_start_date_key = keys[start_dates.index(earliest_start_date)]
+
+        return self.positions[earliest_start_date_key].asset.history.index
